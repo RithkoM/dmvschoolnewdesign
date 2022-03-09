aad3a61b8fe43de962fbf3a18ad527429655294a
RithkoM <49213354+RithkoM@users.noreply.github.com>
2022-03-10 00:34:19 +0300

    Add files via upload

---
+++ html/questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Trafik işaret levhaları ile belirlenmiş yaya ve okul geçitlerinde yaklaşan sürücülerin hangisini yapmaları yanlıştır?",
+		propositionA: "Yavaşlamaları",
+		propositionB: "Hızlarını arttırmakarı",
+		propositionC: "Yayalara geçiş hakkı vermeleri",
+		propositionD: "Okul geçidi gördükleri zaman durmaları ",
+		reponse: "B"
+	},
+
+	{
+		question: "Aşağıdakilerden hangisi, aracın yavaşlaması ve durması hâllerinde diğer araçları ikaz etmek amacıyla yanar?",
+		propositionA: "Sis lambaları",
+		propositionB: "Park lambaları",
+		propositionC: "Fren lambaları",
+		propositionD: "İç aydınlatma lambaları",
+		reponse: "B"
+	},
+
+	{
+		question: "Aşağıdakilerden hangisi ''güvenli sürücülük'' için olumsuz etkiye neden olmaz?",
+		propositionA: "Öfke",
+		propositionB: "Saldırgan sürücülük",
+		propositionC: "Sabırsızlık",
+		propositionD: "Sakin olmak",
+		reponse: "D"
+	},
+
+	{
+		question: "Araçta, aşağıdakilerden hangisinin kullanılması yağışlı havalarda kaza riskini artırır?",
+		propositionA: "Tam şarjlı akü",
+		propositionB: "Uzun yakıt boruları",
+		propositionC: "Eski ve aşınmış lastikler",
+		propositionD: "Antifrizli soğutma suyu",
+		reponse: "C"
+	},
+
+	{
+		question: "Aşağıdakilerden hangisi, kişiye bağlı gelişen trafik kazası nedenlerinden biri değildir?",
+		propositionA: "Olumsuz hava şartları ve yolların bozuk olması",
+		propositionB: "Alkol ve madde bağımlılığı",
+		propositionC: "Uzun yolculuklar ve yorgunluk",
+		propositionD: "Tehlikeye atılmaya hazır kişilik yapısı",
+		reponse: "A"
+	},
+
+	{
+		question: "Motor bloğundaki su kanallarında paslanma ve kireçlenme olmuşsa aşağıdakilerden hangisi meydana gelebilir?",
+		propositionA: "Motorun yağ yakması",
+		propositionB: "Motorun çalışmaması",
+		propositionC: "Motorun zengin karışımla çalışması",
+		propositionD: "Motorun fazla ısınması",
+		reponse: "D"
+	},
+
+	{
+		question: "Çocuklarda yapılan temel yaşam desteği uygulamasında, göğüs kemiği kaç cm aşağı inecek şekilde kalp basısı uygulanır?",
+		propositionA: "2",
+		propositionB: "5",
+		propositionC: "8",
+		propositionD: "11",
+		reponse: "B"
+	},
+
+	{
+		question: "Arkadan çarpma şeklindeki trafik kazalarının en önemli sebebi aşağıdakilerden hangisidir?",
+		propositionA: "Öndeki aracın durması",
+		propositionB: "Öndeki aracın yavaşlaması",
+		propositionC: "Görüş mesafesinin kötü olması",
+		propositionD: "Takip mesafesi kurallarına uyulmaması",
+		reponse: "A"
+	},
+
+	{
+        question : "Öndeki aracı geçmeye başlamadan önce sürücünün ilk yapması gereken nedir?",
+        propositionA : "Geçiş yapacağı yolun güvenli olduğundan emin olmak",
+        propositionB : "Öndeki aracın sağa yaklaşmasını beklemek",
+        propositionC : "Selektör yapmak",
+        propositionD : "Hızını arttırmak",
+        reponse : "A"
+	},
+
+	{ 
+	    question : "Aşağıdaki davranışlardan hangisi trafik adabına sahip bir sürücü için uygundur?",
+        propositionA : "Trafik polisi yoksa kırmızı ışıkta geçmek",
+        propositionB : "Sürekli şerit değiştirerek araç kullanmak",
+        propositionC : "Denetim korkusu olmadan emniyet kemerini takmak",
+        propositionD : "Elektronik denetleme sisteminin olmadığı yerlerde hız limitlerini aşmak",
+		reponse : "C"
+	},
+]
